d7861a535ca3a01738ac8df433e84ef23d7b1a1c
binod bastola <bndbstl@gmail.com>
2023-11-27 15:18:31 +0545

    updated productContainer component

--- client/src/app/components/ProductContainer.js
+++ client/src/app/components/ProductContainer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Image from 'next/image';
 import '../styles/cart.css';
 import { useSelector, useDispatch } from 'react-redux';

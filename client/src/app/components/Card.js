c903f0184e0d7a3d3d0f80a543ec85d5e5af42a0
binod bastola <bndbstl@gmail.com>
2023-11-27 17:27:00 +0545

    Updated the buyNow and addToCart button of card component

--- client/src/app/components/Card.js
+++ client/src/app/components/Card.js
@@ -3,40 +3,74 @@ import { Tooltip } from 'antd';
 import { Rate } from 'antd';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useDispatch ,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { buyNow, addToCart } from "../GlobalRedux/Features/cart.slice.js";
 import { TiTick } from "react-icons/ti";
+import { message } from 'antd';
 
 const Card = ({ details }) => {
 
     const router = useRouter();
     const dispatch = useDispatch();
-    const {productList} = useSelector(state=>state.cart);
-    const [addStatus,setAddStatus]=useState(false);
+    const [messageApi, contextHolder] = message.useMessage();
+    const { productList } = useSelector(state => state.cart);
+    const [addStatus, setAddStatus] = useState(false);
 
-    const handlePurchase = (details) => {
+    const handlePurchase = async(details) => {
+
+          var newData = true;
+
+          await productList.map((items)=>{
+            if(items.id === details._id){
+                router.push('/cart');
+                newData = false;
+            }
+           })
+
+          if(newData){
+          
+            const productInfo = {
+                id: details._id,
+                productName: details.productName,
+                price: details.price,
+                imageUrl: details.imageUrl[0],
+                quantity: 1,
+
+            }
+            dispatch(buyNow(productInfo));
+            setAddStatus(true);
+            router.push('/cart');
+       }
+
+        }
+
+    const handleCart = async(details) => {
+
+        var newData = true;
+    
+        await productList.map((items)=>{
+            if(items.id === details._id){
+                newData = false;
+                messageApi.open({
+                    type: 'error',
+                    content: "Item already exist in the cart",
+                 })
+            }
+           })
+
+       if(newData){
         const productInfo = {
             id: details._id,
             productName: details.productName,
             price: details.price,
             imageUrl: details.imageUrl[0],
             quantity: 1,
-           
+
         }
-      dispatch(buyNow(productInfo));
-      router.push('/cart');
-    }
-    const handleCart= (details) =>{
-        const productInfo = {
-            id: details._id,
-            productName: details.productName,
-            price: details.price,
-            imageUrl: details.imageUrl[0],
-            quantity: 1,
-            
-        }
-         dispatch(addToCart(productInfo));
-         setAddStatus(true);
+        dispatch(addToCart(productInfo));
+        setAddStatus(true);
+       }
+
 
     }
 
@@ -46,6 +80,7 @@ const Card = ({ details }) => {
 
     return (
         <div className='card'>
+            {contextHolder}
             <Tooltip mouseEnterDelay={1} title="Click on the image to view product details">
                 <div className='cardPic' onClick={() => { handleCardClick(details._id) }}>
                     <Image
@@ -63,7 +98,7 @@ const Card = ({ details }) => {
                 <span>Ratings:<Rate disabled value={details.rating} /></span>
                 <div className='cardBtn'>
                     <button onClick={() => handlePurchase(details)} className='buyNow'>Buy now</button>
-                    <button onClick={()=> handleCart(details)} disabled={addStatus} className={addStatus?'addedToCart':'addToCart'}>{addStatus?'Added !':'Add to cart'}</button>
+                    <button onClick={() => handleCart(details)} disabled={addStatus} className={addStatus ? 'addedToCart' : 'addToCart'}>{addStatus ? 'Added !' : 'Add to cart'}</button>
                 </div>
             </div>
         </div>

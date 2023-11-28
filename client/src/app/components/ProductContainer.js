9856c1a06d02850d2f44241f95f24b974f06ebf7
binod bastola <bndbstl@gmail.com>
2023-11-28 12:04:54 +0545

    added a function to change quantity of product in product Container component

--- client/src/app/components/ProductContainer.js
+++ client/src/app/components/ProductContainer.js
@@ -60,6 +60,21 @@ const ProductQty = ({ item }) => {
     dispatch(updateQuantity(updatedList));
   }
 
+  const handleQuantityChange=(itemId,value)=>{
+    const newList = [...productList];
+    const updatedList = newList.map((item,index)=>{
+      if(item.id === itemId){
+        const updatedItem = {...item};
+        updatedItem.quantity= Number(value);
+        return item = updatedItem ;
+      }
+      else{
+        return item;
+      }
+    })
+    dispatch(updateQuantity(updatedList));   
+  }
+
   return (
     <div className='productContainer'>
      <div className='info'>
@@ -83,7 +98,7 @@ const ProductQty = ({ item }) => {
         <span>Product quantity</span>
         <div className='qtyBtn'>
           <button onClick={()=>{handleDecrement(item.id)}}>-</button>
-          <input value={item.quantity} />
+          <input value={item.quantity} onChange={(e)=>handleQuantityChange(item.id,e.target.value)}/>
           <button onClick={()=>handleIncrement(item.id)}>+</button>
         </div>
       </div>

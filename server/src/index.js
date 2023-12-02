43d07a88744c8d7179484d804241c13fed5abcb7
binod bastola <bndbstl@gmail.com>
2023-12-02 19:28:58 +0545

    Aggregated the different user routes into single file

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,6 @@ app.use(cors());
 app.use(express.json({limit: '25mb'}));
 app.use(express.urlencoded({limit: '25mb'}));
 app.use(userRoute);
-app.use(googleUserRoute);
 app.use(productRoute);
 app.use(orderRoute);
 
--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema ({
-    firstName:{type:String,required:true},
-    lastName:{type:String,required:true},
-    phoneNumber:{type:Number,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-  })
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phoneNumber: { type: Number, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+})
 
-const User = mongoose.model('User',userSchema);
+const googleUserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  image: { type: String, required: true },
+})
 
-module.exports=User;
+const User = mongoose.model('User', userSchema);
+const GoogleUser = mongoose.model('GoogleUser', googleUserSchema);
+
+module.exports = User,GoogleUser;

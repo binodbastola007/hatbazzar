e0fae7d9178fd7dcb493bd506513ca6aacaf7543
binod bastola <bndbstl@gmail.com>
2023-12-14 16:19:02 +0545

    Error in esewa form

--- client/src/app/esewa/page.js
+++ client/src/app/esewa/page.js
@@ -1,53 +1,65 @@
 "use client";
-import React,{useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet';
 
 const page = () => {
 
-    const [signature, setSignature] = useState('');
 
-    function signatureGeneration(){
-        var hash = CryptoJS.HmacSHA256(total_amount,transaction_uuid,product_code);
+
+    var currentTime = new Date();
+    var formattedTime = currentTime.toISOString().slice(2, 10).replace(/-/g, '') + '-' + currentTime.getHours() + currentTime.getMinutes() + currentTime.getSeconds();
+
+    const handleSubmit = async () => {
+        var amount = 100;
+        var tax_amount =10;
+        var transaction_uuid = formattedTime;
+        var total_amount = 5000;
+        var product_code = "EPAYTEST";
+        var secret = "8gBm/:&EnhH.1/q";
+
+        var hash = CryptoJS.HmacSHA256(`total_amount=${total_amount},transaction_uuid=${transaction_uuid},product_code=${product_code}`, `${secret}`);
         var hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
-        setSignature(hashInBase64);
-    }
 
-    
-    const amount = "100";
-    const tax_amount = "10";
-    const transaction_uuid = "564654654hjgh";
-    const total_amount = "5000";
-    const product_code = "EPAYTEST";
+
+        const values = {
+            amount: amount,
+            failure_url: "https://google.com",
+            product_delivery_charge: 0,
+            product_service_charge: 0,
+            product_code: product_code,
+            signature: hashInBase64,
+            signed_field_names: "total_amount,transaction_uuid,product_code",
+            success_url: "https://esewa.com.np",
+            tax_amount: tax_amount,
+            total_amount: total_amount,
+            transaction_uuid: transaction_uuid,
+            secret: secret
+        }
+
+ try{
+    const res = await fetch('https://rc-epay.esewa.com.np/api/epay/main/v2/form', {
+        method: 'POST',
+        mode: "no-cors",
+        'Content-Type': 'application/x-www-form-urlencoded',
+        body: JSON.stringify(values)
+    })
+    const data = await res.json();
+    console.log(data);
+    }
+ catch(err){
+    console.log(err);
+ }
+}
 
 
     return (
         <div>
             <Helmet>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1
-                      /crypto-js.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1
-                      /hmac-sha256.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1
-                       /enc-base64.min.js"></script>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>
-                       
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/enc-base64.min.js"></script>
             </Helmet>
-            <button onClick={()=>signatureGeneration()}>Signature Generation button</button>
-            <form action="https://rc-epay.esewa.com.np/api/epay/main/v2/form" method="POST">
-                <input type="text" id="amount" name="amount" value={amount} required />
-                <input type="text" id="tax_amount" name="tax_amount" value={tax_amount} required />
-                <input type="text" id="total_amount" name="total_amount" value={total_amount} required />
-                <input type="text" id="transaction_uuid" name="transaction_uuid" value={transaction_uuid} required />
-                <input type="text" id="product_code" name="product_code" value={product_code} required />
-                <input type="text" id="product_service_charge" name="product_service_charge" value="0" required />
-                <input type="text" id="product_delivery_charge" name="product_delivery_charge" value="0" required />
-                <input type="text" id="success_url" name="success_url" value="https://esewa.com.np" required />
-                <input type="text" id="failure_url" name="failure_url" value="https://google.com" required />
-                <input type="text" id="signed_field_names" name="signed_field_names" value="total_amount,transaction_uuid,product_code" required />
-                <input type="text" id="signature" name="signature" value={signature} required />
-                <input value="Submit" type="submit" />
-            </form>
-
+          <button onClick={()=>handleSubmit()}>Pay with esewa</button>
         </div>
     )
 }
